a1bdc8a95ddd8baaad669cf48d63d42add119b11
wujian <wuguangjian123@gmail.com>
2020-01-10 17:36:25 +0800

    flutter 测试

---
+++ lib/page/fishredux/test/state.dart
@@ -0,0 +1,13 @@
+import 'package:fish_redux/fish_redux.dart';
+
+class RecordState implements Cloneable<RecordState> {
+
+  @override
+  RecordState clone() {
+    return RecordState();
+  }
+}
+
+RecordState initState(Map<String, dynamic> args) {
+  return RecordState();
+}
